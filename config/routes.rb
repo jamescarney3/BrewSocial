0f80dc2c9555c027abbe4f777a5cd586ad25d48c
Your Name <=>
2015-07-14 12:21:31 -0400

    Migrate database, add routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
+  resources :recipes, only: [:new, :edit, :create, :update, :destroy, :show, :index]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714131502) do
+ActiveRecord::Schema.define(version: 20150714155331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.integer  "author_id",  null: false
+    t.string   "title",      null: false
+    t.string   "style",      null: false
+    t.text     "procedure",  null: false
+    t.boolean  "is_private", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "recipes", ["author_id"], name: "index_recipes_on_author_id", using: :btree
+  add_index "recipes", ["style"], name: "index_recipes_on_style", using: :btree
+  add_index "recipes", ["title"], name: "index_recipes_on_title", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
