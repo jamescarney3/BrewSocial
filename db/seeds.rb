81a1c36c3910a7e8f443018ae1a7f3ab7c611d33
Rafael <rafaeljmaldonado@gmail.com>
2015-07-15 17:30:07 -0400

    Add and require composite views library

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,15 @@ Recipe.create(author_id: 1, title: "Fratricide ESB", style: "ESB", procedure: "S
 Recipe.create(author_id: 2, title: "Wall Ice", style: "Ice Lager", procedure: "It's cold up here, so we'd have to skim the ice off whether we wanted to or not. Ferment cold.", is_private: true)
 
 Ingredient.create(name: "Schill Vienna Malt (crushed)", role: 1)
+Ingredient.create(name: "Breiss Caramel Malt (crushed)", role: 1)
+Ingredient.create(name: "Breiss 2-Row Brewer's Malt (crushed)", role: 1)
+Ingredient.create(name: "Maillard Amber Malt (extract syrup)", role: 1)
+Ingredient.create(name: "Munton's Amber Malt (dme)", role: 1)
+
 Ingredient.create(name: "Citra Hops (pellet)", role: 2)
-Ingredient.create(name: "Danstar American West Coast Ale Yest", role: 3)
+Ingredient.create(name: "Equinox Hops (pellet)", role: 2)
+Ingredient.create(name: "Simcoe Hops (whole leaf)", role: 2)
+Ingredient.create(name: "Amarillo Hops (whole leaf)", role: 2)
+
+Ingredient.create(name: "Danstar American West Coast Ale Yeast", role: 3)
+Ingredient.create(name: "White Labs Platinum Strain Yeast", role: 3)
