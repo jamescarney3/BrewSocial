fe2590342a03b3a863acfda03cc62a84e671075d
Rafael <rafaeljmaldonado@gmail.com>
2015-07-15 14:02:50 -0400

    Add brewsocial.js in assets/javascripts

---
+++ app/assets/javascripts/brewsocial.js
@@ -0,0 +1,11 @@
+window.BrewSocial = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function(){
+    // new router here
+    // --> give new router an options hash with collections, a $rootEl
+    Backbone.history.start();
+  }
+};
