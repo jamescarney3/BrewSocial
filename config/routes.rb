ce5606616fc3480c10cff30caa1b84dbeee20eda
Rafael <rafaeljmaldonado@gmail.com>
2015-07-15 14:21:03 -0400

    properly install backbone

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-
 Rails.application.routes.draw do
 
   root to: "static_pages#root"
@@ -6,5 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
   resources :recipes
+
+  # namespace: api, defaults
 
 end
