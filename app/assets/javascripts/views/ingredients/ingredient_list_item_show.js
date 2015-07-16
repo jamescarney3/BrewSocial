18c238894045c244b7bfbaba2749e40d182b8b64
James Carney <jmcarneyiii@gmail.com>
2015-07-16 12:18:07 -0400

    Implement adding and removing of ingredients and subviews from recipe lists and view respectively

--- app/assets/javascripts/views/ingredients/ingredient_list_item_show.js
+++ app/assets/javascripts/views/ingredients/ingredient_list_item_show.js
@@ -1,11 +1,13 @@
 BrewSocial.Views.IngredientListItemShow = Backbone.CompositeView.extend({
   template: JST["ingredients/list_item_show"],
   tagName: "li",
-  initialize: function(){
+  initialize: function(options){
     this.listenTo(this.collection, "sync", this.render);
+    this.subListIndex = options.subListIndex;
   },
   render: function(){
-    var content = this.template({ingredient: this.model});
+    var content = this.template({ingredient: this.model,
+      subListIndex: this.subListIndex});
     this.$el.html(content);
     return this;
   }
--- app/assets/javascripts/views/recipes/recipe_form.js
+++ app/assets/javascripts/views/recipes/recipe_form.js
@@ -4,30 +4,43 @@ BrewSocial.Views.RecipeForm = Backbone.CompositeView.extend({
   events: {
     "click .recipe-submit":"submit",
     "click .add-ingredient":"addIngredient",
-
+    "click .remove-ingredient":"removeIngredient"
   },
   initialize: function(options){
     this.listenTo(this.model, "sync", this.render);
     this.ingredients = options.ingredients;
     this.ingredientList = [];
   },
+
   addIngredient: function(event){
     event.preventDefault();
     var input = this.$el.find("#ingredient-name");
     var addedIngredient = this.ingredients.get(input.val());
-    this.ingredientList.push(addedIngredient);
 
     var newListItemView = new BrewSocial.Views.IngredientListItemShow({
-      model: addedIngredient
+      model: addedIngredient,
+      subListIndex: this.ingredientList.length
     });
 
     this.addSubview("#added-ingredients", newListItemView);
+
+    this.ingredientList.push(addedIngredient);
   },
+
+  removeIngredient: function(event){
+    event.preventDefault();
+    // alert($(event.currentTarget).data("sub-list-index"));
+    var idx = $(event.currentTarget).data("sub-list-index");
+    this.ingredientList.splice(idx, 1);
+    this.subviews("#added-ingredients")._wrapped[idx].remove();
+  },
+
   submit: function(event){
     event.preventDefault();
     var attrs = this.$el.serializeJSON();
     debugger;
   },
+
   render: function(){
     this.ingredients.fetch();
     var content = this.template({recipe: this.model});
