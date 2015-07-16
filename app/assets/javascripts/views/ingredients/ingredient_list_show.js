c66f3e5132c2b09bf4883ccb5f5228a4831efe6f
James Carney <jmcarneyiii@gmail.com>
2015-07-16 10:26:03 -0400

    Add ingredientList array to recipe form view, implement ingredient addition

--- app/assets/javascripts/views/ingredients/ingredient_list_show.js
+++ app/assets/javascripts/views/ingredients/ingredient_list_show.js
@@ -1,11 +1,11 @@
-BrewSocial.Views.IngredientInput = Backbone.CompositeView.extend({
-  template: JST["ingredients/input"],
+BrewSocial.Views.IngredientListItemShow = Backbone.CompositeView.extend({
+  template: JST["ingredients/list_item_show"],
   tagName: "li",
   initialize: function(){
     this.listenTo(this.collection, "sync", this.render);
   },
   render: function(){
-    var content = this.template({ingredients: this.collection});
+    var content = this.template({ingredient: this.model});
     this.$el.html(content);
     return this;
   }
--- app/assets/javascripts/views/recipes/recipe_form.js
+++ app/assets/javascripts/views/recipes/recipe_form.js
@@ -2,26 +2,37 @@ BrewSocial.Views.RecipeForm = Backbone.CompositeView.extend({
   template: JST["recipes/form"],
   tagName: "form",
   events: {
-    "click .recipe-submit":"submit"
+    "click .recipe-submit":"submit",
+    "click .add-ingredient":"addIngredient",
+
   },
   initialize: function(options){
     this.listenTo(this.model, "sync", this.render);
     this.ingredients = options.ingredients;
+    this.ingredientList = [];
+  },
+  addIngredient: function(event){
+    event.preventDefault();
+    var input = this.$el.find("#ingredient-name");
+    var addedIngredient = this.ingredients.get(input.val());
+    this.ingredientList.push(addedIngredient);
+
+    var newListItemView = new BrewSocial.Views.IngredientListItemShow({
+      model: addedIngredient
+    });
+
+    this.addSubview("#added-ingredients", newListItemView);
   },
   submit: function(event){
     event.preventDefault();
     var attrs = this.$el.serializeJSON();
-    console.log(attrs.recipe);
+    debugger;
   },
   render: function(){
     this.ingredients.fetch();
     var content = this.template({recipe: this.model});
     this.$el.html(content);
-    // var sub = new BrewSocial.Views.IngredientInput({collection: this.ingredients});
-    // this.$el.append(sub.render().$el);
-    this.addSubview("#ingredients", new BrewSocial.Views.IngredientInput({collection: this.ingredients}));
-    console.log(this._subviews);
-    debugger;
+    this.addSubview("#ingredient-to-add", new BrewSocial.Views.IngredientInput({collection: this.ingredients}));
     this.attachSubviews();
     return this;
   }
