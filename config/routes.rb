a159df20d334e4ece47159891eac080f20f7eefd
James Carney <jmcarneyiii@gmail.com>
2015-07-20 18:57:30 -0400

    Remove old out-of-use rails-only views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
 
-  resources :users, only: [:new, :create, :show]
-  resource :session, only: [:new, :create, :destroy]
-  resources :recipes
-
   namespace :api, defaults: { format: :json } do
     resources :users
     resources :recipes
