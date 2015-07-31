24f5890b49bbd077ac4ff0da5dab4883b58135c0
James Carney <jmcarneyiii@gmail.com>
2015-07-31 10:23:02 -0400

    Enhance seed data with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,51 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.create(username: "Stannis", password: "password")
-User.create(username: "JonSnow", password: "nothing")
-User.create(username: "Hodor", password: "hodorhodor")
+User.create(username: "Stannis",
+  password: "password",
+  avatar: File.new("#{Rails.root}/app/assets/images/seed_avatars/stannis.png")
+  )
+User.create(username: "JonSnow",
+  password: "nothing",
+  avatar: File.new("#{Rails.root}/app/assets/images/seed_avatars/jonsnow.png")
+  )
+User.create(username: "Hodor",
+  password: "hodorhodor",
+  avatar: File.new("#{Rails.root}/app/assets/images/seed_avatars/hodor.png")
+  )
 User.create(username: "SampleUser", password: "samplepassword")
 
-rec1 = Recipe.create(author_id: 1, title: "Fratricide ESB", style: "ESB", procedure: "Mash at 154F (68C) 60 mins, increase to 168F (76C) 5 minutes, recirculate. Run off wort, sparge with 170F water, boil 90 mins. Equinox & Fuggles at 15 mins, Cascade at 60 mins. Cool & transfer to fermenter, aerate, pitch yeast. Kill Renly with blood magic. Ferment at 70F (21C) until yeast activity diminishes, prime and rack to secondary and bottle when beer falls clear.", is_private: false)
+rec1 = Recipe.create(author_id: 1,
+title: "Fratricide ESB",
+style: "ESB",
+procedure: "Mash at 154F (68C) 60 mins, increase to 168F (76C) 5 minutes, recirculate. Run off wort, sparge with 170F water, boil 90 mins. Equinox & Fuggles at 15 mins, Cascade at 60 mins. Cool & transfer to fermenter, aerate, pitch yeast. Kill Renly with blood magic. Ferment at 70F (21C) until yeast activity diminishes, prime and rack to secondary and bottle when beer falls clear.",
+is_private: false,
+image: File.new("#{Rails.root}/app/assets/images/seed_images/homebrew8.png")
+)
 
-rec2 = Recipe.create(author_id: 2, title: "Night's Watch Eisbock", style: "Eisbock", procedure: "Mash at 152F 90 mins, boil 120 mins. Amarillo at 110 minutes. Transfer to fermenter, add water to bring volume to 5 gallons, cool & pitch yeast. Ferment until yeast activity subsides, defend wall from all-out wildling attack. Lager at 30F 4 weeks, skimming ice regularly, bottle or keg.", is_private: true)
+rec2 = Recipe.create(author_id: 2,
+  title: "Night's Watch Eisbock",
+  style: "Eisbock",
+  procedure: "Mash at 152F 90 mins, boil 120 mins. Amarillo at 110 minutes. Transfer to fermenter, add water to bring volume to 5 gallons, cool & pitch yeast. Ferment until yeast activity subsides, defend wall from all-out wildling attack. Lager at 30F 4 weeks, skimming ice regularly, bottle or keg.",
+  is_private: true,
+  image: File.new("#{Rails.root}/app/assets/images/seed_images/homebrew6.png")
+  )
 
-rec3 = Recipe.create(author_id: 3, title: "Weirwood", style: "Saison", procedure: "Mash grains at 152F for 90 mins, boil fpr 75 mins (Nelson Sauvin at 60 mins, orange and coriander at 70 mins). Cool to room temp, aerate, and add water to bring volume to 5 gal. Primary & secondary ferment 3 weeks each at 75F, prime & condition in swing-top bottles.", is_private: true)
+rec3 = Recipe.create(author_id: 3,
+  title: "Weirwood",
+  style: "Saison",
+  procedure: "Mash grains at 152F for 90 mins, boil fpr 75 mins (Nelson Sauvin at 60 mins, orange and coriander at 70 mins). Cool to room temp, aerate, and add water to bring volume to 5 gal. Primary & secondary ferment 3 weeks each at 75F, prime & condition in swing-top bottles.",
+  is_private: true,
+  image: File.new("#{Rails.root}/app/assets/images/seed_images/homebrew2.png")
+  )
 
-rec4 = Recipe.create(author_id: 1, title: "Blackwater", style: "Schwarzbier", procedure: "Infusion mash at 152F (67C) for 90 mins, boil 90 mins, .67oz Citra 85 mins to 90 mins. Lead disastrous naval attack on capitol city, retreat shamefully. Ferment 2 weeks at 64F, secondary ferment 2 weeks, prime and keg or bottle condition 2 weeks.", is_private: true)
+rec4 = Recipe.create(author_id: 1,
+  title: "Blackwater",
+  style: "Schwarzbier",
+  procedure: "Infusion mash at 152F (67C) for 90 mins, boil 90 mins, .67oz Citra 85 mins to 90 mins. Lead disastrous naval attack on capitol city, retreat shamefully. Ferment 2 weeks at 64F, secondary ferment 2 weeks, prime and keg or bottle condition 2 weeks.",
+  is_private: true,
+  image: File.new("#{Rails.root}/app/assets/images/seed_images/homebrew1.png")
+  )
 
 Ingredient.create(name: "Schill Vienna Malt (crushed)", role: 1)
 Ingredient.create(name: "Breiss Caramel Malt (crushed)", role: 1)
