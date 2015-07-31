c7a6377512d7002e5afadfd0e7e29d08072f57e0
James Carney <jmcarneyiii@gmail.com>
2015-07-31 18:34:08 -0400

    Revise controller for search to exclude private recipes for non-authors

--- app/controllers/api/static_pages_controller.rb
+++ app/controllers/api/static_pages_controller.rb
@@ -11,12 +11,16 @@ class Api::StaticPagesController < ApplicationController
     case params[:search_type]
     when "name"
       @search_results = Recipe.search_by_name(params[:query])
+      scrub_for_privacy
     when "style"
       @search_results = Recipe.search_by_style(params[:query])
+      scrub_for_privacy
     when "ingredient"
       @search_results = Recipe.search_by_ingredients(params[:query])
+      scrub_for_privacy
     when "all"
       @search_results = Recipe.search_by_all(params[:query])
+      scrub_for_privacy
     end
 
     render :recipe_results
@@ -24,6 +28,12 @@ class Api::StaticPagesController < ApplicationController
 
   def search_users
 
+  end
+
+  def scrub_for_privacy
+    @search_results = @search_results.select do |el|
+      el.is_private == false || el.author_id == current_user.id
+    end
   end
 
 end
