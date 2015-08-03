a735e40952854301d3c37c10706665e2691bb10b
James Carney <jmcarneyiii@gmail.com>
2015-08-03 13:28:07 -0400

    Squash search-related bug in user_result jbuilder

--- app/assets/javascripts/views/search_results/show.js
+++ app/assets/javascripts/views/search_results/show.js
@@ -1,6 +1,7 @@
 BrewSocial.Views.SearchResultShow = Backbone.CompositeView.extend({
   template: JST["search_results/show"],
   render: function(){
+    debugger;
     var content = this.template({result: this.model});
     this.$el.html(content);
     return this;
--- app/views/api/static_pages/user_results.json.jbuilder
+++ app/views/api/static_pages/user_results.json.jbuilder
@@ -1,5 +1,5 @@
 json.array! @search_results do |search_result|
-  json.extract! search_result, :username
+  json.extract! search_result, :username, :id
   json.recipe_count search_result.authored_recipes.length
   json.thumb search_result.avatar.url(:thumb)
 end
